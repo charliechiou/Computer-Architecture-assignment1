7399f30e9bd49096968a052092bdf4f428aeea83
charliechiou <charlie910417@gmail.com>
2024-10-06 10:31:19 +0800

    change convolution.m

--- convolution.m
+++ convolution.m
@@ -8,16 +8,43 @@ h = [3 2 1];
 % x = [5 4 1];
 % h = [2 1 1];
 
+% 顯示輸入的向量 x 和 h
+disp('輸入向量 x:');
+disp(x);
+
+disp('輸入向量 h:');
+disp(h);
+
 % 使用 conv 函數計算卷積
 y = conv(x, h);
 
-% 顯示結果
-disp('卷積結果:');
+% 顯示卷積結果
+disp('卷積結果 y:');
 disp(y);
 
-% 繪製卷積結果
+% 繪製 x, h 和卷積結果 y
+figure;
+
+% 第一個圖：顯示向量 x
+subplot(3, 1, 1);
+stem(x, 'filled');
+title('Input x');
+xlabel('Sample points');
+ylabel('Value');
+grid on;
+
+% 第二個圖：顯示向量 h
+subplot(3, 1, 2);
+stem(h, 'filled');
+title('Input h');
+xlabel('Sample points');
+ylabel('Value');
+grid on;
+
+% 第三個圖：顯示卷積結果 y
+subplot(3, 1, 3);
 stem(y, 'filled');
-title('x 和 h 的卷積結果');
-xlabel('樣本點');
-ylabel('值');
+title('Output y');
+xlabel('Sample points');
+ylabel('Value');
 grid on;
