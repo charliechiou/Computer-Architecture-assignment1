b990863b7e129f15d7faed0d66a20fef98feb783
charliechiou <charlie910417@gmail.com>
2024-10-12 09:37:50 +0800

    add convolution_origin.c commits

--- convolution_origin.c
+++ convolution_origin.c
@@ -3,24 +3,27 @@
 
 int main()
 {
-    float x[10] = {1.0, 2.0, 0.0}; // FP16: 1.0, 2.0, 0.0
-    float h[10] = {3.0, 2.0, 1.0}; // FP16: 3.0, 4.0, 1.0
+    // Initialize two sequences
+    float x[10] = {1.0, 2.0, 0.0}; // x: 1.0, 2.0, 0.0
+    float h[10] = {3.0, 2.0, 1.0}; // y: 3.0, 4.0, 1.0
     float y[20];
-    // float x[30] = {0}, h[30] = {0}, y[60] = {0};
-    int i, j, m = 3, n = 3;
+    // initialize two sequence
+    int i, j, m = 3, n = 3; // Lengths of sequences x and h are both 3
 
-    for (i = 0; i < m + n - 1; i++)
+    for (i = 0; i < m + n - 1; i++) // Loop through each position in the output sequence y
     {
-        y[i] = 0;
-        for (j = 0; j <= i; j++)
+        y[i] = 0;                // Initialize the current output sample y[i] to 0
+        for (j = 0; j <= i; j++) // Loop through each element of sequences x and h
         {
+            // Ensure the indices are within bounds for sequences x and h
             if (j < m && (i - j) < n)
-            { 
-                y[i] += x[j] * h[i - j];
+            {
+                y[i] += x[j] * h[i - j]; // calculate
             }
         }
     }
 
+    // Display the output
     printf("Convoluted sequence is :\n");
     for (i = 0; i < m + n - 1; i++)
     {
